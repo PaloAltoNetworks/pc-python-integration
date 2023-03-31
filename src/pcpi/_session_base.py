fe9f9b3d5919dfe575f4ebd39b72778a98a12e77
Adam <ahamiltonsut@paloaltonetworks.com>
2023-03-31 14:46:49 -0700

    best practice update

--- src/pcpi/_session_base.py
+++ src/pcpi/_session_base.py
@@ -393,7 +393,7 @@ class Session:
         if verify == None:
             verify = self.verify
         
-        limit = 20000#Max limit value is 100,000
+        limit = 2000#Max limit value is 100,000
         
         #Force best practices with HS
         json.update({"heuristicSearch": True, "limit": limit, "withResourceJson": True})
